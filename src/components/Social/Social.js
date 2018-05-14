edc4a50e96b8231ab6d773651f21a8cce0e0db93
believelody <believelody@gmail.com>
2018-05-14 05:58:20 +0200

    Update broken link

--- src/components/Social/Social.js
+++ src/components/Social/Social.js
@@ -10,9 +10,9 @@ class Social extends React.Component {
       <div className="isometric">
         <ul>
           <li><a href="https://github.com/believelody" target="_blank"><i class="fab fa-github"></i></a></li>
-          <li><a href="https://bitbucket.org/believelody/"><i class="fab fa-bitbucket"></i></a></li>
-          <li><a href="https://plus.google.com/u/0/"><i class="fab fa-google-plus"></i></a></li>
-          <li><a href="https://www.linkedin.com/in/lody-believe-a755b342/"><i class="fab fa-linkedin-in"></i></a></li>
+          <li><a href="https://bitbucket.org/believelody/" target="_blank"><i class="fab fa-bitbucket"></i></a></li>
+          <li><a href="https://plus.google.com/u/0/" target="_blank"><i class="fab fa-google-plus"></i></a></li>
+          <li><a href="https://www.linkedin.com/in/lody-believe-a755b342/" target="_blank"><i class="fab fa-linkedin-in"></i></a></li>
         </ul>
       </div>
     );
